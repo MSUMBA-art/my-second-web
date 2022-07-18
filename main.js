f363f5ea0e87921506ff38bb606994bf6f3ee9f6
ALEXANDER <msumba83@gmail.com>
2022-07-18 07:19:51 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var typed = new Typed(".typing", {
+    strings: ["Fullstack  Developer", "Software Engineer"],
+    typeSpeed: 85,
+    backSpeed: 85,
+})
+
+// var typed = new Typed(".typing", {
+//   String: ["Web Designer", "Driver"],
+//   typeSpeed: 80,
+//   backSpeed: 80,
+// });
+
+// var typed = new Typed(".typing", {
+//   strings: [".", "Second sentence."],
+//     typeSpeed: 80,
+//     backSpeed: 80,
+// });
+
+var typed = new Typed(".typing-2", {
+  strings: ["Fullstack  Developer", "Software Engineer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+});
